7bf2a072dcd0267a9b4afe59c052fbb8b1e49267
Pablo Deeleman <pablo.deeleman@dynatrace.com>
2020-07-14 03:12:10 +0200

    chore: improve settings readability

--- src/settings.ts
+++ src/settings.ts
@@ -1,8 +1,9 @@
 import { Settings } from './data';
 
 // Main settings file - edit data below at your convenience
-export default {
-  apiUrl: 'https://admin.insights.ubuntu.com/wp-json/wp/v2/posts',
-  pageSize: 3,
-  pageIndex: 0,
-} as Settings;
+const apiUrl =     'https://admin.insights.ubuntu.com/wp-json/wp/v2/posts';  // Remote data endpoint
+const pageSize =    3;                                                       // Items per blog row
+const pageIndex =   0;                                                       // Current page index (zero-based)
+
+// Do not edit anything below this line
+export default { apiUrl, pageSize, pageIndex } as Settings;
