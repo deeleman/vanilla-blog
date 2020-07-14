8e888f7d12f0eb9828e3bdbd690da5c36f203e7b
Pablo Deeleman <pablo.deeleman@dynatrace.com>
2020-07-14 18:48:32 +0200

    test: add test for error component

---
+++ src/ui/error.component.spec.ts
@@ -0,0 +1,19 @@
+import { ErrorComponent } from './error.component';
+
+describe('ErrorComponent', () => {
+  const errorComponent = new ErrorComponent()
+  const errorMock = new Error('Test error mock');
+  const compiledSource = errorComponent.compile(errorMock);
+
+  it('should feature the Error object message in the returned compiled source', () => {
+    expect(compiledSource).toContain(errorMock.message);
+  });
+  
+  it('should feature a BEM error member modifier for the wrapper element', () => {
+    expect(compiledSource).toContain('__error');
+  });
+  
+  it('should feature a Vanilla framework icon in the returned compiled source', () => {
+    expect(compiledSource).toContain('p-icon--error');
+  });
+});
